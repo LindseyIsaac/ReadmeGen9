beb4ce14d2a7113234182872efb002b8c72c8b7c
LindseyIsaac <isaaclindsey89@gmail.com>
2023-04-22 14:58:59 -0500

    smallpush

--- Assets/Js/generateMarkdown.js
+++ Assets/Js/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no license, return an empty string 
+//conditionals
+function renderLicenseBadge(license) {
+  if (license !== "none");
+  else if (boot = bot);
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,10 +15,90 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// this makes it
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [How to Contribute](#howtocontribute)
+* [Test](#test)
+* [License](#license)
+* [Contact](#contact)
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {
+//   if (license !== "none") {
+//     return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+//   }
+//   return "";
+// }
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   if (license !== "none") {
+//     return `\n* [License](#license)\n`;
+//   return ""}
+// }
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
+//   if (license !== "none") {
+//     return `
+//     ## License
+//       Using the ${license} license.
+//       `;
+//   }
+//   return "";
+// }
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+// ${renderLicenseBadge(data.license)}
+// ## Table of Contents
+// * [Description](#description)
+// * [Installation](#installation)
+// * [Usage](#usage)
+// * [Contact-Me](#contact-me)
+// * [Credits](#credits)
+// * [Features](#features)
+// * [Contributors](#contributors)
+// * [test](#test)
+// ${renderLicenseLink(data.license)}
+// ## Description
+// ${data.description}
+// ## Installation
+// ${data.installation}
+// ## Usage
+// ${data.usage}
+// ## Contact-me
+// * Name - ${data.name}
+// * Email - ${data.email}
+// * GitHub - [${data.creator}](https://github.com/${data.creator}/)
+// ## Credits
+// ${data.credits}
+// ## Features
+// ${data.features}
+// ## Contributors
+// ${data.contributors}
+// ## Test
+// ${data.test}
+// ${renderLicenseSection(data.license)}
+// `;
+// }
+
+// module.exports = generateMarkdown;
--- Assets/util/index.js
+++ Assets/util/index.js
@@ -1,13 +1,88 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./Make/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// things a README needs
+const questions = [
+    {
+        type: 'input',
+        name: 'project name',
+        message: 'What is your projects name?',
+      },      
+
+      
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a short description.',
+      },
+  
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the steps required to install your project?',
+      },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Include instructions and screenshots for examples of use.',
+      },
+      //List or checkbox? copied from github as suggested by classmate
+      {
+        type: 'list',
+        name: 'License',
+        message: 'Please select your License.',
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+      },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: 'List your collaborators and link their GitHub profiles, tutorials used, and all third-party assets.',    
+      },
+
+    {
+        type: 'input',
+        name: 'How to Contribute?',
+        message: 'If you want other developers to be able to contribute please add your guidelines on how to here.'
+     },
+     //WHAT ARE YOU?????
+     {
+        type: 'input',
+        name: 'Test',
+        message: 'Provide tests to be included.',
+      },
+      //this is for the user to reach you with additional questions
+      {
+        type: 'input',
+        name: 'Contact',
+        message: 'Please input a valid Email and github name ',
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// generates the readme?
+//NEEDS TO MAKE A PATH I THINK????
+// data.answers for the readme 
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md', generateMarkdown(data), function (err) {
+      if (err) {
+          return console.log(err);
+      }
+  });
+}
+  
+      
 // TODO: Create a function to initialize app
-function init() {}
+// this will start the creation of the readme I think
 
-// Function call to initialize app
-init();
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+      writeToFile("../Make/README.md", generateMarkdown(answers));
+      console.log('answers');
+  });
+}
+// Function call to initialize app 
+//DO I need to write any code here?
+ init();
