03b3476ffff10501d3a0e9d5df398aac18977c68
LindseyIsaac <isaaclindsey89@gmail.com>
2023-04-23 20:13:11 -0500

    working on badges

--- Assets/utils/generateMarkdown.js
+++ Assets/utils/generateMarkdown.js
@@ -2,26 +2,17 @@
 // If there is no license, return an empty string 
 //conditionals
 function renderLicenseBadge(license) {
-  if (license !== 'none') {
-  return `![GitHub license](https://img.shields.io/badge/license-${license}-blueviolet.svg)`;
-}
-return"";
+
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== "none") {
-    return `\n* [License](#license)\n`;
-  }
-  return "";
+ 
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-if (license !== "none") {
-  return `## License Using the ${license} license.`;
-}
-return "";
+
 }
 // TODO: Create a function to generate markdown for README
 // this makes it
